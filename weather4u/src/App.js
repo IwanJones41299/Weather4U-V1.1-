b32b1d73010e4c71dc2632cd5ee4cefa70ddc45d
IwanJones41299 <iwanjones41299@gmail.com>
2020-09-11 16:52:17 +0100

    app functionality complete, just needs turning into a pwa

--- weather4u/src/App.js
+++ weather4u/src/App.js
@@ -2,52 +2,85 @@ import React, { useState } from "react";
 const api = {
   key: "532545037cc7b5bd623fb262091e9651",
   // base: "http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=532545037cc7b5bd623fb262091e9651"
-  base: "http://api.openweathermap.org/data/2.5/"
+  base: "http://api.openweathermap.org/data/2.5/",
 };
 
 function App() {
-
-  const [query, setQuery] = useState('');
+  const [query, setQuery] = useState("");
   const [weather, setWeather] = useState({});
 
-  const search = evt => {
-    if(evt.key === "Enter") {
+  const search = (evt) => {
+    if (evt.key === "Enter") {
       fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
-        .then(res => res.json())
-        .then(result => setWeather(result));
+        .then((res) => res.json())
+        .then((result) => {
+          setWeather(result);
+          setQuery("");
+          console.log(result);
+        });
     }
-  }
+  };
 
   const dateBuilder = (d) => {
-      let months = ["Janauary", "Febuary", "March", "April", "May", "June", "July", "Augyst", "September", "October", "November", "December"];
-      let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Satuday"];
+    let months = [
+      "Janauary",
+      "Febuary",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "Augyst",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Satuday",
+    ];
 
-      let day = days[d.getDay()];
-      let date = d.getDate();
-      let month = months[d.getMonth()];
-      let year = d.getFullYear();
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
 
-      return `${day} ${date} ${month} ${year}`
-  }
+    return `${day} ${date} ${month} ${year}`;
+  };
 
   return (
-    <div className="app">
+    <div className={(typeof weather.main != "undefined" ? ((weather.main.temp > 18) ? 'app warm' : 'app') : 'app')}>
       <main>
         <div className="search-box">
-          <input 
-            type="text" 
-            className="search-bar" 
-            placeholder="Search..." 
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
           />
         </div>
-        <div className="location-box">
-          <div className="location">New York City, US</div>
-          <div className="date">{dateBuilder(new Date())}</div>
-        </div>
-        <div className="weather-box">
-          <div className="temp">15°c</div>
-          <div className="weather">Sunny</div>
-        </div>
+        {typeof weather.main != "undefined" ? (
+          <div>
+            <div className="location-box">
+              <div className="location">
+                {weather.name}, {weather.sys.country}
+              </div>
+              <div className="date">{dateBuilder(new Date())}</div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">{Math.round(weather.main.temp)}°c</div>
+              <div className="weather">{weather.weather[0].main}</div>
+            </div>
+          </div>
+        ) : ("")}
       </main>
     </div>
   );
